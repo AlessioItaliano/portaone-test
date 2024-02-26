26c5a528947deeaf9955cdeb1dcd1a7c448a5676
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2024-02-26 10:44:55 +0200

    add max and min numbers

--- src/components/ResponseLines/ResponseLines.jsx
+++ src/components/ResponseLines/ResponseLines.jsx
@@ -1,27 +1,44 @@
-import { useState } from "react";
+// import { useState } from "react";
 import * as s from "./ResponseLines.styled";
 
 const ResponseLines = ({ newNumbersList }) => {
-  const [maxNumber, setMaxNumber] = useState("");
-  const [minNumber, setMinNumber] = useState("");
-  const [median, setMedian] = useState("");
-  const [averageNumber, setAverageNumber] = useState("");
+  //   const [maxNumber, setMaxNumber] = useState("");
+  //   const [minNumber, setMinNumber] = useState("");
+  //   const [median, setMedian] = useState("");
+  //   const [averageNumber, setAverageNumber] = useState("");
 
   const numbers = newNumbersList.trim().split("\n").map(Number);
 
-  console.log("Масив чисел:", numbers);
+  console.log(numbers);
 
-  setMaxNumber(1);
-  setMinNumber(1);
-  setMedian(1);
-  setAverageNumber(1);
+  let maxNumber = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maxNumber) {
+      maxNumber = numbers[i];
+    }
+  }
+
+  let minNumber = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < minNumber) {
+      minNumber = numbers[i];
+    }
+  }
+
+  const addDots = (number) => {
+    const parts = number.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return parts.join(".");
+  };
 
   return (
     <s.Container>
-      <p>1. The maximum number in the file : {maxNumber}</p>
-      <p>2. The minimum number in the file : {minNumber}</p>
-      <p>3. The median : {median}</p>
-      <p>4. Average arithmetic value : {averageNumber}</p>
+      <p>1. The maximum number is : {addDots(maxNumber)}</p>
+      <p>2. The minimum number is: {addDots(minNumber)}</p>
+      {/* <p>3. The median : {median}</p> */}
+      {/* <p>4. Average arithmetic value : {averageNumber}</p> */}
     </s.Container>
   );
 };
